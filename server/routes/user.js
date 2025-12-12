99080f40fdb1d87ccf231219bc05cafdb1f6acaa
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-13 02:06:28 +0530

    feat(auth): create auth routes file with register, login, logout

---
+++ server/routes/user.js
@@ -0,0 +1,17 @@
+const {Router}=require('express');
+const {handleUserRegister, handleUserLogin, handleUserLogout, checker}=require('../controllers/user');
+const { userAuthorization } = require('../middleware/userAuthorization');
+
+const route=Router()
+
+//route for registering new user
+route.post('/api/auth/register',handleUserRegister)
+
+//route for user login
+route.post('/api/auth/login',handleUserLogin)
+
+//route for logging user out
+route.get('/api/auth/logout',userAuthorization,handleUserLogout)
+
+
+module.exports=route
