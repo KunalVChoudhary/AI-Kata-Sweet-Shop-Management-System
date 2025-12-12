afc22e6980ecea5446c65b0fed6f844f0ce8506f
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-13 01:49:38 +0530

    refactor(app): move express initialization to app.js fr om server.js and  keep server startup in server.js

--- server/server.js
+++ server/server.js
@@ -1,27 +1,5 @@
-require('dotenv').config()
-const express = require('express');
-const cookieParser=require('cookie-parser')
-const cors = require('cors');
-const { connectDb } = require('./services/databaseConnection');
-
-//initializing express app/server
-const app = express();
-
-//connection MongoDB
-connectDb()
-
-//middlewares
-app.use(express.json());
-app.use(cookieParser(`${process.env.COOKIE_PARSER_SECRET_KEY}`));
-
-//cors 
-app.use(cors({
-  origin: [`${process.env.CLIENT_URL}`],
-  credentials: true,
-}));
-
+require("dotenv").config();
+const app = require("./src/app");
 
 //staring server
 app.listen(process.env.PORT,()=>{console.log('Server Started');})
-
-module.exports = app;
