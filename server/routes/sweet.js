92e05c0d91d6f6df427dff9b19ee18c0f14adef5
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-13 16:20:10 +0530

    feat (sweets): created routes to perform create, update, delete operations on sweet (admin-only) and also to fetch sweets by users

--- server/routes/sweet.js
+++ server/routes/sweet.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { createSweet } = require('../controller/sweet');
+const { createSweet, getAllSweets, searchSweets, updateSweet, deleteSweet } = require('../controller/sweet');
 const { userAuthorization } = require('../middleware/userAuthorization');
 const { adminAuthorization } = require('../middleware/adminAuthorization');
 
@@ -7,5 +7,17 @@ const router = Router();
 
 //protected route to let admin create asweet item in db
 router.post('/api/sweets', userAuthorization, adminAuthorization, createSweet);
+
+//route for fetching the list of available sweets
+router.get('/api/sweets', getAllSweets)
+
+//route for fetching the list of sweets based on filter params
+router.get('/api/sweets/search', searchSweets);
+
+//route for Updating a sweet's details
+router.put('/api/sweets/:id', userAuthorization, adminAuthorization, updateSweet)
+
+//route for deleting a sweet (admin only)
+router.delete('/api/sweets/:id', userAuthorization, adminAuthorization, deleteSweet)
 
 module.exports = router;
