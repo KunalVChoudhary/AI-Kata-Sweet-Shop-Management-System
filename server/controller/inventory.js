680a9fa4d04adc7763c0e78e34ab4785070362fe
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-13 17:50:00 +0530

    feat(sweets): cerated controller function  to purchase sweet and update inventory

---
+++ server/controller/inventory.js
@@ -0,0 +1,48 @@
+const Sweet = require('../models/sweet');
+const User = require('../models/user');
+
+//controller function for  POST /api/sweets/:id/purchase to manage users purchase request
+const purchaseSweet = async (req, res) => {
+    try {
+
+        //check if user is logged in
+        if (!req.user || !req.user.userId) {
+            return res.status(400).json({ message: 'Please Login' });
+        }
+        const user = await User.findById(req.user.userId)
+        req.user=user
+
+        //role check
+        if (!req.user || req.user.role !== 'USER') {
+            return res.status(400).json({ message: 'Only USER can purchase a sweets' });
+        }
+
+        const { id } = req.params;
+
+        //check if any sweeet exists with this id
+        const sweet = await Sweet.findById(id);
+        if (!sweet) {
+        return res.status(400).json({ message: 'Sweet not found' });
+        }
+
+        if (sweet.quantity <= 0) {
+        return res.status(400).json({ message: 'Sweet out of stock' });
+        }
+
+        sweet.quantity = sweet.quantity - 1;
+        await sweet.save();
+
+        return res.status(200).json({
+        id: sweet._id,
+        name: sweet.name,
+        quantity: sweet.quantity
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ message: 'Could not purchase sweet' });
+    }
+};
+
+module.exports = {
+  purchaseSweet
+};
