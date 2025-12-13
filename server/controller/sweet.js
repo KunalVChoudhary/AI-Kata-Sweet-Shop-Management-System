14bcf6d661420013f9a4df8d7edfd656eba6d367
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-13 14:51:33 +0530

    feat(sweets): add controller to get list of all available sweets based on searching parameters

--- server/controller/sweet.js
+++ server/controller/sweet.js
@@ -43,4 +43,38 @@ const getAllSweets = async (req, res) => {
   }
 };
 
-module.exports = { createSweet, getAllSweets};
+
+// GET /api/sweets/search
+const searchSweets = async (req, res) => {
+  try {
+    const { name, category, minPrice, maxPrice } = req.query;
+
+    const query = {};
+
+    // search by name (partial, case-insensitive)
+    if (name) {
+      query.name = { $regex: name, $options: 'i' };
+    }
+
+    // search by category
+    if (category) {
+      query.category = category;
+    }
+
+    // search by price range
+    if (minPrice || maxPrice) {
+      query.price = {};
+      if (minPrice) query.price.$gte = Number(minPrice);
+      if (maxPrice) query.price.$lte = Number(maxPrice);
+    }
+
+    const sweets = await Sweet.find(query);
+
+    return res.status(200).json(sweets);
+  } catch (err) {
+    return res.status(400).json({ message: 'Could not search sweets' });
+  }
+};
+
+
+module.exports = { createSweet, getAllSweets, searchSweets};
