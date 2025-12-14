6e19d385ce25e2ed2dadff5d8b365e7f6e5066a3
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-14 15:54:19 +0530

    feat(pages): add register page with auth/login form and styles

---
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,18 @@
+import styles from './RegisterPage.module.scss'
+import LoginForm from '../../components/LoginForm/LoginForm'
+import { ToastContainer } from 'react-toastify'
+
+function RegisterPage({purpose}) {
+  return (
+    <>
+      <div>
+        <ToastContainer position="top-right" />
+        <div className={`${styles['login-form-container']} d-flex align-items-center justify-content-center`}>
+          <LoginForm purpose={purpose} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default RegisterPage
