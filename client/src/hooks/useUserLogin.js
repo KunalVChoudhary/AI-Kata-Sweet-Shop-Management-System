e9a64ddb3c4cca5fb5796391dcf5cac0c06ec455
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-14 12:20:04 +0530

    feat(auth): created custom hook for user login and registration

---
+++ client/src/hooks/useUserLogin.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAuth } from "./useAuth";
+
+//custom hook/function to handle users login/register requests
+export default function useUserLogin(){
+    const [loading, setLoading] = useState(false)
+    const {user, setUser} = useAuth()
+    const {role, setRole} = useState('USER')
+    const navigate = useNavigate();
+
+    const loginUser = async (purpose,loginData)=>{
+        setLoading(true)
+        try{
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/${(purpose === 'Sign Up')? 'register' : 'login'}`,{
+            method : 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData),
+            credentials: 'include'
+        })
+        const data = await response.json()
+        if (response.ok){
+            toast.success(data.message)
+            setUser(data.username)
+            setRole(data.role)
+            setTimeout(()=> navigate('/'),2000)
+        } else if (response.status===400){
+            toast.error(data.message)
+        } else {
+            toast.error(data.message)
+        }}
+        catch(error){
+            toast.error('Error! Try Again')
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    return { loginUser, loading, user, role };
+
+}
