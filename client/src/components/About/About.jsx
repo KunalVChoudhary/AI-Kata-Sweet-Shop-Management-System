4edee6b26778f0d393d691ab3e46897abbf31051
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-12-14 21:39:54 +0530

    feat(ui): created about component layout

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom';
+import styles from './About.module.scss';
+import Card from '../Card/Card';
+
+function About() {
+
+  const [searchParams,setSearchParams] = useSearchParams();
+  const [sweetsArray, setSweetsArray] = useState([]);
+
+
+  useEffect(() => {
+    const query = searchParams.toString();
+    const baseUrl = import.meta.env.VITE_API_URL;
+
+    const url = query
+    ? `${baseUrl}/api/sweets/search?${query}`  // filters or search params present
+    : `${baseUrl}/api/sweets`;  
+
+    fetch(url,{
+      method:'GET',credentials:'include'
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setSweetsArray(data)
+      })
+    }, [searchParams]);
+
+
+    return (
+      <div className={`${styles['sweetListContainer']} p-3` }>
+        <div className={`${styles['sweetCardContainer']} p-3`}>
+          {sweetsArray.map((element,index)=>{
+            return <Card key={index} setSweetsArray={setSweetsArray} cardDetail={element}/>
+          })}
+        </div>
+      </div>
+    )
+  }
+
+export default About
